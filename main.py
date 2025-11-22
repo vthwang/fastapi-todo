221f18bec2730c68b193bd665fb312703478d80c
vthwang <vincent@fishboneapps.com>
2025-11-22 15:57:19 -0800

    feat: get a single todo
    Signed-off-by: vthwang <vincent@fishboneapps.com>

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, Depends
+from fastapi import FastAPI, Depends, HTTPException, Path
 import models
 from models import Todo
 from database import engine, SessionLocal
@@ -20,6 +20,15 @@ def get_db():
 
 db_dependency = Annotated[Session, Depends(get_db)]
 
+
 @app.get("/")
 def read_all(db: db_dependency):
     return db.query(Todo).all()
+
+
+@app.get("/todo/{todo_id}")
+def read_todo(db: db_dependency, todo_id: int = Path(gt=0)):
+    todo_model = db.query(Todo).filter(Todo.id == todo_id).first()
+    if todo_model is not None:
+        return todo_model
+    raise HTTPException(status_code=404, detail="Todo not found")
