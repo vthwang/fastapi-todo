233b01425ef222ff108976cccf22c3cdab194fe4
vthwang <vincent@fishboneapps.com>
2025-11-22 16:37:26 -0800

    feat: hash the password
    Signed-off-by: vthwang <vincent@fishboneapps.com>

--- routers/auth.py
+++ routers/auth.py
@@ -1,8 +1,47 @@
 from fastapi import APIRouter
+from pydantic import BaseModel
+from database import SessionLocal
+from sqlalchemy.orm import Session
+from typing import Annotated
+from fastapi import Depends
+from models import User
+from passlib.context import CryptContext
 
 router = APIRouter()
 
+bcrypt_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+
+db_dependency = Annotated[Session, Depends(get_db)]
+
+
+class CreateUserRequest(BaseModel):
+    username: str
+    email: str
+    first_name: str
+    last_name: str
+    password: str
+    role: str
+
 
 @router.post("/auth")
-def get_user():
-    return {"id": 1, "username": "testuser", "email": "testuser@example.com"}
+def create_user(create_user_request: CreateUserRequest, db: db_dependency):
+    user_model = User(
+        username=create_user_request.username,
+        email=create_user_request.email,
+        first_name=create_user_request.first_name,
+        last_name=create_user_request.last_name,
+        password=bcrypt_context.hash(create_user_request.password),
+        role=create_user_request.role,
+        is_active=True,
+    )
+    db.add(user_model)
+    db.commit()
