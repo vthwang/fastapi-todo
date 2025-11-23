53e170d69d86defc45ff59542e496a6d1ff1d441
vthwang <vincent@fishboneapps.com>
2025-11-22 16:00:30 -0800

    feat: post a todo
    Signed-off-by: vthwang <vincent@fishboneapps.com>

--- main.py
+++ main.py
@@ -1,13 +1,22 @@
 from fastapi import FastAPI, Depends, HTTPException, Path
 import models
+from pydantic import BaseModel, Field
 from models import Todo
 from database import engine, SessionLocal
 from sqlalchemy.orm import Session
 from typing import Annotated
+from starlette import status
 
 app = FastAPI()
 
 models.Base.metadata.create_all(bind=engine)
+
+
+class TodoRequest(BaseModel):
+    title: str = Field(min_length=3)
+    description: str = Field(min_length=3, max_length=100)
+    priority: int = Field(gt=0, lt=6)
+    complete: bool
 
 
 def get_db():
@@ -32,3 +41,10 @@ def read_todo(db: db_dependency, todo_id: int = Path(gt=0)):
     if todo_model is not None:
         return todo_model
     raise HTTPException(status_code=404, detail="Todo not found")
+
+
+@app.post("/todo", status_code=status.HTTP_201_CREATED)
+def create_todo(db: db_dependency, todo_request: TodoRequest):
+    todo_model = Todo(**todo_request.model_dump())
+    db.add(todo_model)
+    db.commit()
