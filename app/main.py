0f380e89166dc09af9603f54c815d4a32db880a2
vthwang <vincent@fishboneapps.com>
2025-11-23 17:09:55 -0800

    feat: add scalar
    Signed-off-by: vthwang <vincent@fishboneapps.com>

--- app/main.py
+++ app/main.py
@@ -2,14 +2,25 @@ from fastapi import FastAPI
 from .models import Base
 from .database import engine
 from .routers import auth, todos, admin, users
+from scalar_fastapi import get_scalar_api_reference
 
 app = FastAPI()
 
 Base.metadata.create_all(bind=engine)
 
+
 @app.get("/healthy")
 def healthy():
     return {"status": "healthy"}
+
+
+@app.get("/scalar", include_in_schema=False)
+def scalar_html():
+    return get_scalar_api_reference(
+        openapi_url=app.openapi_url,
+        scalar_proxy_url="https://proxy.scalar.com",
+    )
+
 
 app.include_router(admin.router)
 app.include_router(auth.router)
